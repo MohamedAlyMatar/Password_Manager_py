7f6475305c61fffa661eaa40daf6852b123b83a6
“Mohamed <mohamedalymatar2001@gmail.com>
2023-07-10 17:45:08 +0300

    added colors for pass status

--- functions.py
+++ functions.py
@@ -8,15 +8,38 @@ password_patterns = {
     "diamond": r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=])[a-zA-Z\d@#$%^&+=]{8,}$"
 }
 
-def add_password():
-    account = input("Enter account name: ")
-    password = input("Enter password: ")
+def check_password_level(password):
+    for level, pattern in password_patterns.items():
+        if re.match(pattern, password):
+            return level
+    return "Unknown"
 
-    if validate_password(password):
+def print_colored_status(status):
+    colors = {
+        "weak": "\033[91m",  # Red color
+        "strong": "\033[93m",  # Yellow color
+        "diamond": "\033[92m",  # Green color
+        "Unknown": "\033[0m"  # Reset color
+    }
+    color_code = colors.get(status, "")
+    print("Status:", color_code + status + "\033[0m")
+
+def get_password():
+    password = input("Enter your password: ")
+    level = check_password_level(password)
+    print_colored_status(level)
+    return password, level
+
+def add_password():
+    account = input("Enter the account name: ")
+    password, level = get_password()
+
+    if validate_password(password, level):
         passwords[account] = password
         print("Password added successfully!")
     else:
         print("Invalid password.")
+
 
 def retrieve_password():
     account = input("Enter the account name: ")
@@ -26,13 +49,14 @@ def retrieve_password():
     else:
         print(f"No password found for {account}.")
 
+
 def update_password():
     account = input("Enter the account name: ")
 
     if account in passwords:
-        password = input("Enter the new password: ")
+        password, level = get_password()
 
-        if validate_password(password):
+        if validate_password(password, level):
             passwords[account] = password
             print("Password updated successfully!")
         else:
@@ -40,8 +64,10 @@ def update_password():
     else:
         print(f"No password found for {account}.")
 
-def validate_password(password):
-    for pattern_name, pattern in password_patterns.items():
-        if re.match(pattern, password):
-            return True
-    return False
+
+def validate_password(password, level):
+    if level in password_patterns:
+        pattern = password_patterns[level]
+        return re.match(pattern, password) is not None
+    else:
+        return False
